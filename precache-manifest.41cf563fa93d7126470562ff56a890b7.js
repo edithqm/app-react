e06d240f2aacbc2cb0ada68003fb72cb8371406b
edith stefany <edistefan416@gmail.com>
2020-08-21 11:00:49 -0500

    Updates

---
+++ precache-manifest.41cf563fa93d7126470562ff56a890b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eed174c3b7a6caf69ddab24bc55673a4",
+    "url": "/edithqm/app-react.git/index.html"
+  },
+  {
+    "revision": "dda55cbea90b65108439",
+    "url": "/edithqm/app-react.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b61657c882f704081a94",
+    "url": "/edithqm/app-react.git/static/js/2.d008f8a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/edithqm/app-react.git/static/js/2.d008f8a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dda55cbea90b65108439",
+    "url": "/edithqm/app-react.git/static/js/main.15195380.chunk.js"
+  },
+  {
+    "revision": "d2730bcb331bcffd318b",
+    "url": "/edithqm/app-react.git/static/js/runtime-main.6f8dd547.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/edithqm/app-react.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
